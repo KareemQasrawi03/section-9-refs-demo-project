65b29d713d9bb83e74f019fdaa495571e245d8e0
Kareem Alqasarweh <kareemalqasarweh@LPJOR0161.local>
2025-10-07 14:03:06 +0300

    Initial commit

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Button from "./Button";
+import img from "../assets/no-projects.png"
+
+
+function NoProjectSelected({ onStartAddProject }) {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        src={img}
+        alt="no imge"
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h2 className="text-xl font-boldtext-stone-500 my-5 ">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">
+        Selecte a Project or get started with a new one
+      </p>
+      <p className="mt-8">
+        <Button onClick={onStartAddProject}>Create new project</Button>
+      </p>
+    </div>
+  );
+}
+
+export default NoProjectSelected;
